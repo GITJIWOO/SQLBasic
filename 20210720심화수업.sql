b7e52af189b6fefae2a2bf3006ad5418f351f041
KIMJIWOO <85823060+GITJIWOO@users.noreply.github.com>
2021-07-21 13:52:44 +0900

    Add files via upload

--- 20210720심화수업.sql
+++ 20210720심화수업.sql
@@ -461,15 +461,164 @@ ALTER TABLE wbtbl DROP COLUMN money;
 ALTER TABLE wbtbl CHANGE COLUMN job ujob VARCHAR(20)
 	DEFAULT '1';
 
+
+-- <제약조건>
+-- 컬럼에 붙는 조건으로, 데이터의 무결성을 보장하기 위해
+-- 추가적으로 붙이는 제한을 의미합니다.
+-- 제약조건이 붙은 컬럼에는 제약조건을 만족시키는 데이터만
+-- 집어넣을 수 있기 때문에 개발자가 의도한 데이터만
+-- 들어오는 장점이 있습니다.
+
+-- 제약조건 확인
+-- DESC, DESCRIBE
+-- DESC DB명.테이블명;
+DESC employees.employees;
+
+
 -- 제약조건 추가
--- NOT NULL, UNIQUE 등
--- ALTER TABLE 테이블명 MODIFY 컬럼명 데이터타입 제약조건;
-ALTER TABLE wbtbl MODIFY ujob VARCHAR(20) UNIQUE;
+-- PRIMARY KEY
+-- ALTER TABLE 테이블명 ADD CONSTRAINT 제약조건 명칭 PRIMARY KEY(컬럼이름);
+CREATE TABLE testTbl5 (
+	num INT,
+    name VARCHAR(5)
+);
+ALTER TABLE testTbl5 ADD CONSTRAINT pk_tbl5num PRIMARY KEY(num);
+
+DESC testTbl5;
+
+-- FOREIGN KEY
+-- ALTER TABLE 테이블명 ADD CONSTRAINT 제약조건명
+-- FOREIGN KEY(컬럼명) REFERENCES 연결테이블(PK컬럼);
+CREATE TABLE testTbl6 (
+	fnum INT NOT NULL,
+    num INT PRIMARY KEY,
+    id VARCHAR(5)
+);
+ALTER TABLE testTbl6 ADD CONSTRAINT fk_fnum
+FOREIGN KEY(fnum) REFERENCES testTbl5(num);
+
+DESC testTbl6;
 
 -- 제약조건 삭제
 -- 일부 제약조건은 구문이 조금 다릅니다.
--- ALTER TABLE 테이블명 DROP 제약조건명;
-ALTER TABLE wbtbl DROP NOT NULL;
+-- PRIMARY KEY
+-- ALTER TABLE 테이블명 DROP PRIMARY KEY;
+ALTER TABLE testTbl5 DROP PRIMARY KEY;
+
+-- FOREIGN KEY
+-- ALTER TABLE 테이블명 DROP FOREIGN KEY 제약조건명;
+ALTER TABLE testTbl6 DROP FOREIGN KEY fk_fnum;
+
+-- 프로시저는 그냥 호출할 수도 있지만, 파라미터를 입력받게
+-- 처리해야 훨씬 사용성이 높아집니다.
+-- 따라서 프로시저의 성질에 따라 파라미터를 활용할 수 있도록
+-- 파라미터를 추가한 프로시저를 정의해보겠습니다.
+-- 파라미터 지정은
+-- CREATE PROCEDURE 프로시저명(
+--             IN 파라미터명 자료형(크기)
+-- )
+-- 와 같은 형식으로 지정할 수 있습니다.
+-- 예시 프로시저
+DELIMITER $$
+CREATE PROCEDURE singerAge(
+	IN userName VARCHAR(10)
+)
+BEGIN
+	DECLARE bYear INT;
+    SELECT birthYear INTO bYear FROM userTbl
+		WHERE name = userName;
+	IF (bYear >= 1980) THEN
+		SELECT '젊은 편입니다.' as '결과';
+	ELSE
+		SELECT '나이가 좀 있습니다.' as '결과';
+	END IF;
+END $$
+DELIMITER ;
+
+CALL singerAge('이승기');
+
+-- 어제 작성한 employees.getFive() 프로시저를
+-- 사원번호를 입력받아서 입력받은 사원의 근속년수가
+-- 5년이 넘는지 안넘는지 판단하는 프로시저로 고쳐서
+-- getFive2() 로 만들어주시고 호출도 해 주세요.
+USE employees;
+
+DESC employees;
+DELIMITER $$
+CREATE PROCEDURE getFive2(
+	IN number INT
+)
+BEGIN
+	DECLARE hireDATE DATE;
+    DECLARE todayDATE DATE;
+    DECLARE days INT;
+    SELECT hire_date INTO hireDATE
+		FROM employees WHERE emp_no = number;
+	SET todayDATE  = CURRENT_DATE();
+    SET days = DATEDIFF(todayDATE, hireDATE);
+    IF (days / 365) >= 5 THEN
+		SELECT days as '근속 일수';
+	ELSE
+		SELECT '근속 년수가 5년보다 적습니다.';
+	END IF;
+END $$
+DELIMITER ;
+
+CALL getFive2(10001);
+
+-- <트리거>
+-- 트리거는 특정 테이블에 명령이 내려지면 자동으로
+-- 연동된 명령을 수행하도록 구문을 걸어주는 것입니다.
+-- 예를들어 회원이 탈퇴를 했다고 가정했을 때
+-- 탈퇴한 회원을 DB에서 바로 삭제하는게 아니라
+-- 탈퇴보류회원 테이블에 INSERT를 해 둔다던지
+-- 수정이 일어났을 때 수정 전 내역을 따로 다른 테이블에
+-- 저장해 둔다던지 특정 테이블을 대상으로 실행되는 구문 이전이나
+-- 혹은 이후에 추가로 실행할 명령어를 지정해두는 것입니다.
+
+-- 문법은 프로시저와 유사하나, 차이점이 조금 있습니다.
+-- 문법)
+-- DELIMITER //
+-- CREATE TRIGGER 트리거명
+--         실행시점 실행로직
+--         ON 트리거부착테이블
+--         FOR EACH ROW
+-- BEGIN
+--     트리거 실행시 작동코드
+-- END //
+-- DELIMITER ;
+
+-- 트리거 테스트용 테이블
+CREATE TABLE gameList (
+    id INT,
+    gamename VARCHAR(10)
+);
+INSERT INTO gameList VALUES(1, '바람의 나라');
+INSERT INTO gameList VALUES(2, '스타크래프트');
+INSERT INTO gameList VALUES(3, '롤');
+SELECT * FROM gameList;
+
+-- 트리거를 gameList 테이블에 부착합니다.
+DELIMITER //
+CREATE TRIGGER testTrg
+    AFTER DELETE
+    ON gameList
+    FOR EACH ROW
+BEGIN
+    SET @msg = '게임이 삭제되었습니다.';
+END //
+DELIMITER ;
+
+SELECT @msg = '';
+INSERT INTO gameList VALUES(4, '배틀 그라운드');
+SELECT @msg;
+
+SET sql_safe_updates = 0;
+UPDATE gameList SET gamename = '리그 오브 레전드'
+    WHERE id = 3;
+SELECT @masg;
+DELETE FROM gameList WHERE id = 4;
+SELECT @msg;
 
 
 COMMIT;
